45353f7a5a20b5c90faa2f0149074bb07bc6aceb
siddharth sharma <sharmasiddharth9717@gmail.com>
2025-06-03 14:10:15 +0530

    redefine this the statement correctly

--- testing/test1.java
+++ testing/test1.java
@@ -3,7 +3,7 @@ package testing;
 public class test1 {
     public static void main(String[] args) {
         int age = 17 ;
-        if (age<8){
+        if (age<=18){
             System.out.println("your are under the age of 18-- years " );
         }
         else {
